6cb35c282c31c1822e85c846e7b0baed0eb97c4c
NaphatKPS <6430098021@student.chula.ac.th>
2022-11-17 22:12:26 +0700

    a little bit Skill

--- src/entity/base/Skill.java
+++ src/entity/base/Skill.java
@@ -2,23 +2,44 @@ package entity.base;
 
 import java.util.ArrayList;
 
-public abstract class Skill {
+public abstract class Skill implements Countdownable {
 	private int ratio;
 	private String name;
 	private String text;
 	private int cd;
 	private ArrayList<Buff> buffs;
+	private boolean isAOE;
 	
-	public Skill(String name,String text,int ratio,int cd) {
+	private int inCombatCd;
+	
+	
+	public Skill(String name,String text,int ratio,int cd,boolean isAOE) {
 		this.setName(name);
 		this.setText(text);
 		this.setRatio(ratio);
 		this.setCd(cd);
+		this.setAOE(isAOE);
 		
 	}
 	
 	
 	public  abstract void skillActive(ArrayList<Unit> units, Unit targetUnit);
+	
+	
+	public void useSkill( ArrayList<Unit> units, Unit targetUnit ) {
+		this.skillActive(units, targetUnit);
+		this.setInCombatCd(this.cd);
+	}
+	
+	
+	public void countDown() {
+		this.setInCombatCd(inCombatCd - 1);
+	}
+	
+	
+	public boolean readySkill() {
+		return this.inCombatCd == 0;
+	}
 	
 	
 	// getter setter
@@ -60,6 +81,30 @@ public abstract class Skill {
 
 	public void setCd(int cd) {
 		this.cd = cd;
+	}
+
+
+	public int getInCombatCd() {
+		return inCombatCd;
+	}
+
+
+	public void setInCombatCd(int inCombatCd) {
+		if(inCombatCd<0) {
+			this.inCombatCd = 0;
+			return;
+		}
+		this.inCombatCd = inCombatCd;
+	}
+
+
+	public boolean isAOE() {
+		return isAOE;
+	}
+
+
+	public void setAOE(boolean isAOE) {
+		this.isAOE = isAOE;
 	}
 	
 	
--- src/skill/type/AttackSkill.java
+++ src/skill/type/AttackSkill.java
@@ -1,5 +1,22 @@
 package skill.type;
 
-public class AttackSkill {
+import java.util.ArrayList;
+
+import entity.base.Skill;
+import entity.base.Unit;
+
+public class AttackSkill extends Skill {
+
+	public AttackSkill(String name, String text, int ratio, int cd, boolean isAOE) {
+		super(name, text, ratio, cd, isAOE);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public void skillActive(ArrayList<Unit> units, Unit targetUnit) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
