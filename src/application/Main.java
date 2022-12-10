a32b011d9cf3bca7541c552be5ce5c52d1ccd28d
zan <puzan_s@hotmail.com>
2022-12-10 15:47:11 +0700

    adding gui scene , some audio and image , edit some part of main .

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,10 +3,20 @@ package application;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.management.Notification;
+
+import org.junit.platform.launcher.Launcher;
+
 import entity.base.Unit;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import logic.GameLogic;
 
-public class Main {
+public class Main extends Application  {
 	
 	private static Scanner keyBoard;
 	//private static int Distance;
@@ -14,33 +24,56 @@ public class Main {
 	private static boolean isCombatMode;
 	private static boolean isHeroTurn;
 	
-	public static void main(String[] args) {
-		
-		
-		keyBoard = new Scanner(System.in);
-		
-		while(true){
-			System.out.println("=============================");
-			System.out.println("Welcome to");
-			System.out.println("Overdue! All You Can Rush");
-			System.out.println("=============================");
-			System.out.println("What are you doing?");
-			System.out.println("1) Start Game");
-			System.out.println("2) Quit");
-			System.out.println("=============================");
-			int results = getChoice();
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/gui/MenuScene.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Progmeth-project");
+			primaryStage.setResizable(false);
+			primaryStage.show();
 			
-			if(results==1) {
-				//System.out.println("=============================");
-				startGame();
-			}else if(results==2){
-				break;
-			}else {
-				System.out.println("Invalid Input, Terminate the game.");
-				break;
-			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+
+
+//	public static void main(String[] args) {
+//		
+//		
+//		keyBoard = new Scanner(System.in);
+//		
+//		while(true){
+//			System.out.println("=============================");
+//			System.out.println("Welcome to");
+//			System.out.println("Overdue! All You Can Rush");
+//			System.out.println("=============================");
+//			System.out.println("What are you doing?");
+//			System.out.println("1) Start Game");
+//			System.out.println("2) Quit");
+//			System.out.println("=============================");
+//			int results = getChoice();
+//			
+//			if(results==1) {
+//				//System.out.println("=============================");
+//				startGame();
+//			}else if(results==2){
+//				break;
+//			}else {
+//				System.out.println("Invalid Input, Terminate the game.");
+//				break;
+//			}
+//		}
+//	}
 	
 	public static int getChoice() {
 		System.out.print(">> ");
@@ -155,6 +188,8 @@ public class Main {
 		}
 		//ArrayList
 	}
+
+
 	
 
 }
--- src/gui/MenuController.java
+++ src/gui/MenuController.java
@@ -1,5 +1,49 @@
 package gui;
 
-public class MenuController {
+import java.io.IOException;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MenuController  {
+
+	public Stage stage ;
+	public Scene scene ;
+	public Parent root ;
+	
+	public void switchToCombat (ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("/gui/CombatScene.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void switchToMenu (ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("/gui/MenuScene.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void exitGame (ActionEvent event) {
+		Platform.exit();
+		System.exit(0);
+	}
 }
